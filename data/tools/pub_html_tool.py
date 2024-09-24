527e9ee677e53e078bbe925f733b48200a015f83
GGN-2015 <premierbob@qq.com>
2024-09-24 20:39:43 +0800

    v2024-09-24

--- data/tools/pub_html_tool.py
+++ data/tools/pub_html_tool.py
@@ -2,6 +2,19 @@ import markdown2
 import re
 import pub_dir_utils
 import pub_mylog
+import base64
+import string
+import secrets
+
+def generate_random_sequence(length): 
+    characters = string.ascii_lowercase + string.ascii_uppercase + string.digits # 定义字符集，包括小写字母、大写字母和数字
+    random_sequence = ''.join(secrets.choice(characters) for _ in range(length)) # 使用 secrets.choice 来生成随机序列
+    return random_sequence
+
+# 中介常量：必须是随机的字母数字序列
+RAW_DOLLAR         = generate_random_sequence(64)
+MATH_CONTENT_BEGIN = generate_random_sequence(64)
+MATH_CONTENT_END   = generate_random_sequence(64)
 
 CDN = """
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
@@ -35,33 +48,13 @@ def wrap_http_link(md_text):
         new_text = new_text.replace(old_link, new_link)
     return new_text
 
-# 对数学公式中的 "\" 字符数量进行翻倍
-def pre_math_keep_slash_type1(md_text):
-    regex    = r"\$[^\$\n]+\$"
+# 把数学块内容保护起来
+def wrap_math_content(md_text):
+    regex = r"\$+[^\$]+\$+"
     new_text = md_text
     for match in re.finditer(regex, md_text):
-        old_link = (match.group(0))
-        new_link = (match.group(0)).replace("\\", "\\\\")
-        new_text = new_text.replace(old_link, new_link)
-    return new_text
-
-# 对数学公式中的 "\" 字符数量进行翻倍
-def pre_math_keep_slash_type2(md_text):
-    regex    = r"\$\$[^\$]+\$\$"
-    new_text = md_text
-    for match in re.finditer(regex, md_text):
-        old_link = (match.group(0))
-        new_link = (match.group(0)).replace("\\", "\\\\")
-        new_text = new_text.replace(old_link, new_link)
-    return new_text
-
-# 对数学公式中的小于号和大于号进行转义
-def pre_math_keep_lt_gt(md_text):
-    regex    = r"\$\$[^\$]+\$\$"
-    new_text = md_text
-    for match in re.finditer(regex, md_text):
-        old_link = (match.group(0))
-        new_link = (match.group(0)).replace("<", r"\lt ").replace(">", r"\gt ")
+        old_link = match.group(0)
+        new_link = MATH_CONTENT_BEGIN + base64.b64encode(old_link.encode("utf-8")).decode("utf-8") + MATH_CONTENT_END
         new_text = new_text.replace(old_link, new_link)
     return new_text
 
@@ -75,6 +68,12 @@ def rename_link(md_text):
         new_text = new_text.replace(old_link, new_link)
     return new_text
 
+# 保护美元符号
+def wrap_raw_dollar(md_text):
+    new_text = md_text
+    new_text = new_text.replace(r"\$", RAW_DOLLAR)
+    return new_text
+
 # 渲染删除线
 def render_del(md_text):
     regex = "~~[^~]*~~"
@@ -85,24 +84,25 @@ def render_del(md_text):
         new_text = new_text.replace(old_link, new_link)
     return new_text
 
-# 渲染行内数学公式
-def render_math_in_line(html_text):
-    regex    = r"[^\$](\$[^\$\n]+\$)[^\$]"
-    new_text = html_text
-    for match in re.finditer(regex, html_text):
-        old_link = (match.group(1))
-        new_link = ("\\(" + match.group(1)[1:-1] + "\\)").replace("<em>", "_").replace("</em>", "_")
-        new_text = new_text.replace(old_link, new_link)
+# 还原受保护的美元符号
+def unwrap_raw_dollar(html_content):
+    new_text = html_content
+    new_text = new_text.replace(RAW_DOLLAR, "$")
     return new_text
 
-# 删除数学公式中的 <em>
-def del_em_in_math_content(html_text):
-    regex    = r"\$\$[^\$]+\$\$"
-    new_text = html_text
-    for match in re.finditer(regex, html_text):
+# 从保护数据中反解出数学块内容
+def unwrap_math_content(html_content):
+    regex = MATH_CONTENT_BEGIN + r"[a-z0-9A-Z\=\+\-\/]+" + MATH_CONTENT_END
+    new_text = html_content
+    for match in re.finditer(regex, html_content):
         old_link = (match.group(0))
-        new_link = (match.group(0)).replace("<em>", "_").replace("</em>", "_")
-        new_text = new_text.replace(old_link, new_link)
+        new_link = old_link.replace(MATH_CONTENT_BEGIN, "").replace(MATH_CONTENT_END, "").strip()
+        new_link = base64.b64decode(new_link).decode("utf-8")
+        if new_link.startswith("$$"):
+            new_text = new_text.replace(old_link, new_link)
+        else:
+            # 假设以一个 $ 开头
+            new_text = new_text.replace(old_link, "\\(" +new_link[1:-1] + "\\)")
     return new_text
 
 # 为所有 markdown 文件制作 html 副本
@@ -112,14 +112,13 @@ def create_all_html_file():
         assert old_file.endswith(".md")
         markdown_content = open(old_file, encoding="utf-8").read()
         markdown_content = wrap_http_link(markdown_content) # 渲染管线
-        markdown_content = pre_math_keep_slash_type1(markdown_content)
-        markdown_content = pre_math_keep_slash_type2(markdown_content)
-        markdown_content = pre_math_keep_lt_gt(markdown_content)
-        markdown_content = rename_link(markdown_content)
         markdown_content = render_del(markdown_content)
+        markdown_content = rename_link(markdown_content)
+        markdown_content = wrap_raw_dollar(markdown_content)
+        markdown_content = wrap_math_content(markdown_content)
         html_content     = get_html_from_md(markdown_content) # 渲染 html
-        html_content     = render_math_in_line(html_content)
-        html_content     = del_em_in_math_content(html_content)
+        html_content     = unwrap_math_content(html_content) # 渲染 html
+        markdown_content = unwrap_raw_dollar(html_content)
         new_file = old_file[:-3] + ".html"
         open(new_file, "w", encoding="utf-8").write(html_content)
 
