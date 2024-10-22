246a80d00c822e3f1793b1406bc1e56f3557dd21
GGN_2015 <premierbob@qq.com>
2024-10-22 16:23:01 +0800

    v2024-10-22

--- data/tools/pub_auto_pipeline.py
+++ data/tools/pub_auto_pipeline.py
@@ -2,12 +2,18 @@ import pub_copy_std
 import pub_gen_line_stat
 import pub_gen_title_list
 import pub_html_tool
+import pub_neko_interface
 
-def auto_pipeline(): # 自动化构建过程
+def render_public():
     pub_copy_std.copy_std()
     pub_gen_line_stat.gen_line_stat_file()
     pub_gen_title_list.create_title_list_file()
     pub_html_tool.create_all_html_file()
+
+def auto_pipeline(): # 自动化构建过程
+    render_public()
+    pub_neko_interface.neko("send_blog") # 远程调用公有仓库中的代码
+    pub_neko_interface.neko("push")
 
 if __name__ == "__main__":
     auto_pipeline()
---
+++ data/tools/pub_neko_interface.py
@@ -0,0 +1,7 @@
+# 在共有仓库调用私有仓库中的 neko 命令
+# 建议只做 bash 脚本调用，不要调用具体实现细节
+import subprocess
+
+def neko(*args) -> int:
+    result = subprocess.run(['neko'] + list(args), capture_output=True, text=True)
+    return result.returncode
